9f39ce638792ab57a41aa2a891c92874e7553d44
rarcard <qwerdfbg123456@163.com>
2019-10-25 11:26:29 +0800

    源码

---
+++ src/main/java/com/example/demo/controller/wp/LoginController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller.wp;
+
+import com.example.demo.entity.Candidate;
+import com.example.demo.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/wp")
+public class LoginController {
+    @Autowired
+    private CandidateService candidateService;
+    @RequestMapping("/login")
+    public String Login(Model model,@RequestParam(defaultValue = "-1")String i){
+        if(i.equals("1")){
+            model.addAttribute("msg","验证码错误");
+        }else if(i.equals("0")){
+            model.addAttribute("msg","账户密码错误");
+        }
+        return "wp/login";
+    }
+    @RequestMapping("/tologin")
+    public String WpLogin(HttpServletRequest request, String account, String password, String code, RedirectAttributes redirectAttributes){
+        String v= (String) request.getSession().getAttribute("CODE");
+        if(code.equals(v)){
+            Candidate candidate=candidateService.WpLogin(account,password);
+            if(candidate==null){
+                redirectAttributes.addAttribute("i",0);
+                return "redirect:login";
+            }else{
+                return "redirect:HomePage";
+            }
+        }else {
+            redirectAttributes.addAttribute("i",1);
+            return "redirect:login";
+        }
+
+    }
+}
